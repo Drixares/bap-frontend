2924912f2b43f72b1bcb5fb832270a2bc5e3fd59
Matteo <matteo.lorenzoq@gmail.com>
2024-12-03 10:58:42 +0100

    chore: transformed projects list and authors list to components

--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -6,10 +6,10 @@ import authors from "../datas/fake/search-authors.json";
 import { ProjectWithAuthorsType } from "../types/project";
 import SearchSwitcher from "../components/search/search-switcher";
 import { DisplayType } from "../types/search";
-import Project from "../components/home/project";
 import { AuthorType } from "../types/author";
-import AuthorCard from "../components/search/author-card";
 import AuthorPopup from "../components/search/author-popup";
+import SearchProjectsList from "../components/search/search-projects-list";
+import SearchAuthorsList from "../components/search/search-authors-list";
 
 const SearchPage = () => {
     const [display, setDisplay] = useState<DisplayType>("projects");
@@ -19,7 +19,7 @@ const SearchPage = () => {
         <AppLayout>
             <header className="flex flex-col items-center justify-center gap-14 pt-32 px-4">
                 <div className="flex flex-col items-center gap-8 z-30">
-                    <h1 className="text-6xl font-bold text-slate-950">
+                    <h1 className="text-start md:text-center text-6xl font-bold text-slate-950">
                         Explore our innovatives projects
                     </h1>
                     <p className="text-3xl font-medium text-slate-800">
@@ -31,22 +31,13 @@ const SearchPage = () => {
             <div className="w-full max-w-screen-2xl mx-auto px-4 mt-64 flex flex-col gap-12">
                 <SearchSwitcher display={display} setDisplay={setDisplay} />
                 {display === "projects" && (
-                    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-5 gap-y-10">
-                        {projects.map((project: ProjectWithAuthorsType, index: number) => (
-                            <Project key={`project:${index}`} project={project} />
-                        ))}
-                    </div>
+                    <SearchProjectsList projects={projects as ProjectWithAuthorsType[]} />
                 )}
                 {display === "authors" && (
-                    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-x-5 gap-y-10">
-                        {authors.map((author: AuthorType, index: number) => (
-                            <AuthorCard
-                                key={`author:${index}`}
-                                author={author}
-                                setSelectedAuthor={setSelectedAuthor}
-                            />
-                        ))}
-                    </div>
+                    <SearchAuthorsList
+                        authors={authors as AuthorType[]}
+                        setSelectedAuthor={setSelectedAuthor}
+                    />
                 )}
             </div>
             {selectedAuthor && (
