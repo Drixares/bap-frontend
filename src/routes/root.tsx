1721d5740eea8e9c4a71a7e07620516f9baa0f8f
Mattéo <matteo.lorenzoq@gmail.com>
2024-12-16 09:34:50 +0100

    chore: destructuring components

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -9,89 +9,14 @@ import { CategoryType } from "../types/category";
 import SocialProof from "../components/home/social-proof";
 import { TextEffectPerWord } from "../components/ui/text-effect-per-word";
 import AnimatedHeading1 from "../components/home/heading1";
-import ArtificialLives from "../assets/svg/artificialLives.svg";
-import HumanLearning from "../assets/svg/humanLearning.svg";
-import ResilientFutures from "../assets/svg/resilientFutures.svg";
-import { Transition, Variants } from "motion/react";
-import AnimatedLogo from "../components/home/animated-logos";
-import { motion } from "motion/react";
-
-const containerVariants: Variants = {
-    initial: {
-        rotate: 0,
-    },
-    rotate: {
-        rotate: 360,
-        transition: {
-            duration: 1.5,
-            type: "spring",
-            bounce: 0.2,
-            bounceDamping: 1,
-            delay: 2.5,
-        },
-    },
-};
-
-const variants: Variants = {
-    initial: {
-        scale: 0,
-        opacity: 0,
-    },
-    grow: {
-        scale: 1,
-        opacity: 1,
-        transition: {
-            duration: 0.5,
-            type: "spring",
-            stiffness: 260,
-            damping: 20,
-            delay: 1.4,
-        },
-    },
-    counterRotate: {
-        rotate: -360,
-        transition: {
-            duration: 1.5,
-            type: "spring",
-            bounce: 0.2,
-            bounceDamping: 1,
-            delay: 2.5,
-        },
-    },
-};
-
-const moveLogosTransition: Transition = {
-    duration: 0.5,
-    type: "spring",
-    bounce: 0.2,
-    bounceDamping: 1,
-    delay: 2,
-};
-
-const moveLogosPositions = [
-    {
-        x: -130,
-        y: -130,
-        transition: moveLogosTransition,
-    },
-    {
-        x: 70,
-        y: -90,
-        transition: moveLogosTransition,
-    },
-    {
-        x: -90,
-        y: 80,
-        transition: moveLogosTransition,
-    },
-];
+import AnimatedLogoContainer from "../components/home/animated-logo-container";
 
 function Root() {
     return (
         <AppLayout>
             <header className="flex flex-col xl:flex-row justify-between w-full px-4 py-[110px] mx-auto max-w-screen-2xl">
                 <div className="shrink-0 max-w-screen-xl">
-                    <TextEffectPerWord delay={0.52}>
+                    <TextEffectPerWord delay={0.6} preset="fadeBottom">
                         Institutive for Future Technologies
                     </TextEffectPerWord>
                     <AnimatedHeading1 />
@@ -100,32 +25,7 @@ function Root() {
                         <ArrowRight />
                     </ButtonLink>
                 </div>
-                <motion.div
-                    className="hidden xl:block relative w-full origin-center "
-                    style={{
-                        transformOrigin: "center center",
-                    }}
-                    variants={containerVariants}
-                    initial="initial"
-                    animate="rotate"
-                >
-                    <AnimatedLogo
-                        logo={{ srcImg: ArtificialLives, title: "Artificial Lives" }}
-                        variants={variants}
-                        move={moveLogosPositions[0]}
-                    />
-                    <AnimatedLogo
-                        logo={{ srcImg: HumanLearning, title: "Human Learning" }}
-                        variants={variants}
-                        move={moveLogosPositions[1]}
-                    />
-                    <AnimatedLogo
-                        logo={{ srcImg: ResilientFutures, title: "Resilient Futures" }}
-                        variants={variants}
-                        move={moveLogosPositions[2]}
-                    />
-                    {/* <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-5 rounded-full bg-red-500" /> */}
-                </motion.div>
+                <AnimatedLogoContainer />
                 {/* <img
                     src={Globe}
                     alt=""
